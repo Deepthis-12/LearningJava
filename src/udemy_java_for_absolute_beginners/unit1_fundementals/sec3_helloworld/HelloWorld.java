fedc2cae566dd190ef789d44daa5e83377d7b12a
Deepthis-12 <deepthi.surapaneni86@gmail.com>
2025-05-07 11:50:01 +0200

    Added packages

---
+++ src/udemy_java_for_absolute_beginners/unit1_fundementals/sec3_helloworld/HelloWorld.java
@@ -0,0 +1,7 @@
+package udemy_java_for_absolute_beginners.unit1_fundementals.sec3_helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
