277690de3ad1aa76eda6ac187a41e16e9ede5db4
Deepthis-12 <deepthi.surapaneni86@gmail.com>
2025-05-12 22:12:24 +0200

    If condition

--- src/learn/java/udemy/javaforabsolutebeginners/unit1/fundamentals/sec3/helloworld/notes_helloworld.md
+++ src/learn/java/udemy/javaforabsolutebeginners/unit1/fundamentals/sec3/helloworld/notes_helloworld.md
@@ -1,5 +1,7 @@
 1) Naming conventions
- - Class Names : First letter Capitalised and camel case. 
+ - Class Names : First letter Capitalised (Pascal case). 
    - Eg, public class "ConditionalStatements" {}
  - Variable names: First letter small and then camel case. Variable names ("age","name") and variable types(int, String)are different
    - Eg, int "ageOfPerson" ;
+- Package names: All words small letter
+- Eg, package com.company.mypackage ;
---
+++ src/learn/java/udemy/javaforabsolutebeginners/unit1/fundamentals/sec6/conditionalstatements/ConditionalStatements.java
@@ -0,0 +1,17 @@
+package learn.java.udemy.javaforabsolutebeginners.unit1.fundamentals.sec6.conditionalstatements;
+
+import java.util.Scanner;
+
+public class ConditionalStatements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to roller coaster !!");
+        System.out.print("Enter you height in centimeters: ");
+        int heightInCentimeters = scanner.nextInt();
+        if (heightInCentimeters > 180){
+            System.out.println("You are allowed !");
+        }else{
+            System.out.println("You are not allowed !");
+        }
+    }
+}
